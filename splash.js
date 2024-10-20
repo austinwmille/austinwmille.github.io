ee2774d4a67617355e4e4150d4abf3900c606933
austinwmille <austinwmille@gmail.com>
2024-10-20 19:30:10 -0400

    fixed everything
    updated header and footer on all old pages. the splash text is now its own js file and shown on all old pages. css for old and new pages are updated to look better on mobile. headers now don't run over to new lines on any device afaik

---
+++ splash.js
@@ -0,0 +1,40 @@
+// splash.js
+const randomTexts = [
+    "technically online",
+    "Powered by artificial intelligence!",
+    "Runs on beer and cats!",
+    "Astrophysicist technically!",
+    "A total loser",
+    "Certified sadboi!",
+    "hacker, sort of",
+    "Your minecraft boss!",
+    "In love <3",
+    "404 time not found",
+    "stop blaming the Jews.",
+    "iFunny=gayyyyyy",
+    "Happy Chrimbus!",
+    "string theory is wack fr",
+    "will die by my own hand",
+    "suicidal",
+    "420 blaze it",
+    "astrologers, just...idk",
+    "I hate everyone",
+    "medicated",
+    "clinically depressed :(",
+    "goddoesnotwearabuttplug",
+    "actually not gay",
+    "theif",
+    "death to the bourgeoisie!",
+    "power to the proletariat!",
+    "Objectively cooler than you",
+    "only 59 warnings and 19 errors!"
+    "bruh.",
+    "kick ass and chew bubblegum",
+];
+
+document.addEventListener("DOMContentLoaded", function() {
+    const splashElement = document.getElementById("splashtxt");
+    if (splashElement) {
+        splashElement.innerText = randomTexts[Math.floor(Math.random() * randomTexts.length)];
+    }
+});
